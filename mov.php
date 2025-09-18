5a65029ee24257a74c5cc30312c3a57a43b92f3b
evtprojetos2 <78711411+evtprojetos2@users.noreply.github.com>
2025-09-18 11:28:46 -0300

    Add files via upload

---
+++ mov.php
@@ -0,0 +1,17 @@
+{
+    "series": "http://sinalprivado.info/player_api.php?username=430214&password=430214&action=get_series&category_id=",
+    "episodios": "http://sinalprivado.info/player_api.php?username=430214&password=430214&action=get_series_info&series_id=",
+    "play": "http://sinalprivado.info/series/430214/430214/",
+    "filmes": "http://sinalprivado.info/player_api.php?username=430214&password=430214&action=get_vod_streams&category_id=",
+    "categoria": "http://sinalprivado.info/player_api.php?username=430214&password=430214&action=get_vod_categories",
+    "player": "http://sinalprivado.info/movie/430214/430214/",
+    "detalhes_filmes": "http://sinalprivado.info/player_api.php?username=430214&password=430214&action=get_vod_info&vod_id=",
+    "canais": "http://sinalprivado.info/player_api.php?username=430214&password=430214&action=get_live_streams&category_id=",
+    "cate_canais": "http://sinalprivado.info/player_api.php?username=430214&password=430214&action=get_live_categories",
+    "player_iptv": "http://sinalprivado.info/live/430214/430214/",
+    "top": "33",
+    "lanca": "23",
+    "animacao": "22",
+    "familia": "3494",
+    "ficcao": "3483"
+}
